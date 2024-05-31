300d65de0a786a98ac3c71aad987796e239b1660
qylWheels <command1748165360@126.com>
2024-05-31 11:25:35 +0000

    Finish task: take drop tail.

--- Chapter3/Exercises/take_and_drop/main.ml
+++ Chapter3/Exercises/take_and_drop/main.ml
@@ -11,3 +11,20 @@ let rec drop n lst =
     match lst with
     | [] -> []
     | _ :: t -> drop (n - 1) t
+
+let rec string_of_list = function
+| [] -> ""
+| h :: t -> string_of_int h ^ string_of_list t
+
+let ( -- ) a b = List.init (b - a + 1) (fun i -> i + a)
+
+let rec take_aux n lst acc =
+  if n = 0 then List.rev acc
+  else
+    match lst with
+    | [] -> List.rev acc
+    | h :: t -> take_aux (n - 1) t (h :: acc)
+
+let take_tr n lst = take_aux n lst []
+
+let drop_tr n lst = drop n lst
--- Chapter3/Exercises/take_and_drop/test.ml
+++ Chapter3/Exercises/take_and_drop/test.ml
@@ -10,6 +10,13 @@ let tests = "test suite" >::: [
   "drop(): non-empty list, n = 0" >:: (fun _ -> assert_equal [1; 2; 3] (drop 0 [1; 2; 3]));
   "drop(): length of list is larger than n" >:: (fun _ -> assert_equal [3] (drop 2 [1; 2; 3]));
   "drop(): length of list is less than n" >:: (fun _ -> assert_equal [] (drop 3 [1; 2]));
+  "take_tr(): empty list, n > 0" >:: (fun _ -> assert_equal [] (take_tr 100 []));
+  "take_tr(): non-empty list, n = 0" >:: (fun _ -> assert_equal [] (take_tr 0 [1; 2; 3]));
+  "take_tr(): length of list is larger than n"
+    >:: (fun _ -> assert_equal [1; 2] (take_tr 2 [1; 2; 3]));
+  "take_tr(): length of list is less than n" >:: (fun _ -> assert_equal [1; 2] (take_tr 3 [1; 2]));
+  "take_tr(): deal with list having length 10_000_000"
+    >:: (fun _ -> assert_equal (0 -- 9_999_999) (take_tr 10_000_000 (0 -- 10_000_000)))
 ]
 
 let _ = run_test_tt_main tests
