b0f5d917fcaf3427b047eea351400bb7cb4dea35
qylWheels <command1748165360@126.com>
2024-05-31 07:14:25 +0000

    Finish task: library puzzle.

--- Chapter3/Exercises/library/library.ml
+++ Chapter3/Exercises/library/library.ml
@@ -3,3 +3,9 @@ let fifth l =
   else 0
 
 let desc_sort l = List.sort Stdlib.compare l |> List.rev
+
+let last l = List.nth l ((List.length l) - 1)
+
+let rec any_zeroes = function
+  | [] -> false
+  | h :: t -> h = 0 || any_zeroes t
--- Chapter3/Exercises/library/test.ml
+++ Chapter3/Exercises/library/test.ml
@@ -7,7 +7,12 @@ let tests = "library test suite" >::: [
   "fifth(): element count equal to 5" >:: (fun _ -> assert_equal 5 (fifth [1; 2; 3; 4; 5]));
   "fifth(): element count more than 5" >:: (fun _ -> assert_equal 5 (fifth [1; 2; 3; 4; 5; 6; 7]));
   "desc_sort(): empty list" >:: (fun _ -> assert_equal [] (desc_sort []));
-  "desc_sort(): arbitrary" >:: (fun _ -> assert_equal [6; 5; 4; 3] (desc_sort [4; 3; 6; 5]));
+  "desc_sort(): arbitrary list" >:: (fun _ -> assert_equal [6; 5; 4; 3] (desc_sort [4; 3; 6; 5]));
+  "last(): arbitrary list" >:: (fun _ -> assert_equal 5 (last [3; 8; 5]));
+  "any_zeroes(): empty list" >:: (fun _ -> assert_equal false (any_zeroes []));
+  "any_zeroes(): list without zero" >:: (fun _ -> assert_equal false (any_zeroes [3; 6; 114514]));
+  "any_zeroes(): list with one zero" >:: (fun _ -> assert_equal true (any_zeroes [0; 6; 114514]));
+  "any_zeroes(): list with multiple zero" >:: (fun _ -> assert_equal true (any_zeroes [0; 6; 0]));
 ]
 
 let _ = run_test_tt_main tests
