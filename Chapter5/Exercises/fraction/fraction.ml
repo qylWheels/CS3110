ee505d96fd376cec79b994a9f2870dbb71db14cc
qylWheels <command1748165360@126.com>
2024-06-07 12:52:33 +0000

    Finish task: fraction reduced.

--- Chapter5/Exercises/fraction/fraction.ml
+++ Chapter5/Exercises/fraction/fraction.ml
@@ -18,6 +18,13 @@ end
 module Fraction : FractionSig = struct
   type t = int * int
   exception ZeroDenominator
+
+  (** [gcd x y] is the greatest common divisor of [x] and [y].
+    Requires: [x] and [y] are positive. *)
+  let rec gcd x y =
+    if x = 0 then y
+    else if (x < y) then gcd (y - x) x
+    else gcd y (x - y)
   let make n d =
     if d = 0 then raise ZeroDenominator
     else (n, d)
@@ -26,6 +33,19 @@ module Fraction : FractionSig = struct
   let to_string = function (n, d) -> Printf.sprintf "%i/%i" n d
   let to_float = function (n, d) -> (float_of_int n) /. (float_of_int d)
   let add t1 t2 =
-    match (t1, t2) with ((n1, d1), (n2, d2)) -> make (n1 * d2 + n2 * d1) (d1 * d2)
-  let mul t1 t2 = match (t1, t2) with ((n1, d1), (n2, d2)) -> make (n1 * n2) (d1 * d2)
+    match (t1, t2) with
+    | ((n1, d1), (n2, d2)) -> begin
+      let new_n = n1 * d2 + n2 * d1 in
+      let new_d = d1 * d2 in
+      let common_divisor = gcd new_n new_d in
+      make (new_n / common_divisor) (new_d / common_divisor)
+    end
+  let mul t1 t2 =
+    match (t1, t2) with
+    | ((n1, d1), (n2, d2)) -> begin
+      let new_n = n1 * n2 in
+      let new_d = d1 * d2 in
+      let common_divisor = gcd new_n new_d in
+      make (new_n / common_divisor) (new_d / common_divisor)
+    end
 end
--- Chapter5/Exercises/fraction/test.ml
+++ Chapter5/Exercises/fraction/test.ml
@@ -7,13 +7,14 @@ let tests = "test suite for fraction" >::: [
   "denominator" >:: (fun _ -> assert_equal 2 (Fraction.(make 1 2 |> denominator)));
   "to_string" >:: (fun _ -> assert_equal "1/2" (Fraction.(make 1 2 |> to_string)));
   "to_float" >:: (fun _ -> assert_equal 0.5 (Fraction.(make 1 2 |> to_float)));
-  "add" >:: (fun _ -> assert_equal (Fraction.make 4 4) (
+  "add" >:: (fun _ -> assert_equal (Fraction.make 1 1) (
     let frac = Fraction.make 1 2 in
     Fraction.add frac frac
   ));
-  "mul" >:: (fun _ -> assert_equal (Fraction.make 1 4) (
-    let frac = Fraction.make 1 2 in
-    Fraction.mul frac frac
+  "mul" >:: (fun _ -> assert_equal (Fraction.make 1 3) (
+    let frac1 = Fraction.make 2 3 in
+    let frac2 = Fraction.make 1 2 in
+    Fraction.mul frac1 frac2
   ));
 ]
 
