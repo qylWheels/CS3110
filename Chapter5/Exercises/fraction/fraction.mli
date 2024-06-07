72421ec9df39ec1b7049ebfc83a47c372b6406c2
qylWheels <command1748165360@126.com>
2024-06-07 06:54:14 +0000

    Add module type.

---
+++ Chapter5/Exercises/fraction/fraction.mli
@@ -0,0 +1,15 @@
+module type Fraction = sig
+  (* A fraction is a rational number p/q, where q != 0. *)
+  type t
+
+  (** [make n d] is n/d. Requires d != 0. *)
+  val make : int -> int -> t
+
+  val numerator : t -> int
+  val denominator : t -> int
+  val to_string : t -> string
+  val to_float : t -> float
+
+  val add : t -> t -> t
+  val mul : t -> t -> t
+end
