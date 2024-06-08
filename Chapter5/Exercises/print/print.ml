2c3f000eaabf1b69c4265586033166dada26012d
qylWheels <command1748165360@126.com>
2024-06-08 05:33:08 +0000

    Finish task: Print Int.

--- Chapter5/Exercises/print/print.ml
+++ Chapter5/Exercises/print/print.ml
@@ -6,3 +6,10 @@ end
 module Print (M : ToString) = struct
   let print v = v |> M.to_string |> print_endline
 end
+
+module Int = struct
+  type t = int
+  let to_string = string_of_int
+end
+
+module PrintInt = Print(Int)
