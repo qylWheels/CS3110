6c2768a08ad552fdda82a1d78cffd310d08502dc
qylWheels <command1748165360@126.com>
2024-08-04 13:14:45 +0000

    Finish task: lazy sequence

--- Chapter8/Exercises/MyLazy/src/mylazy.ml
+++ Chapter8/Exercises/MyLazy/src/mylazy.ml
@@ -4,3 +4,14 @@ let ( &&& ) a b =
   let a_result = Lazy.force a in
   if a_result = false then false
   else Lazy.force b
+
+type 'a lazysequence = Cons of 'a * 'a lazysequence Lazy.t
+
+let rec map f seq =
+  let Cons (h, t) = seq in
+  Cons (f h, lazy (map f (Lazy.force t)))
+
+let rec filter p seq =
+  let Cons (h, t) = seq in
+  if p h then Cons (h, lazy (filter p (Lazy.force t)))
+  else filter p (Lazy.force t)
