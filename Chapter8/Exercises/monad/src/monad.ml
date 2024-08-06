423145f87be76d3a17ac5c6213bf0652593b6c5b
qylWheels <command1748165360@126.com>
2024-08-06 14:17:57 +0000

    Finish task: add opt

---
+++ Chapter8/Exercises/monad/src/monad.ml
@@ -0,0 +1,23 @@
+module type Monad = sig
+  type 'a t
+  val return : 'a -> 'a t
+  val ( >>= ) : 'a t -> ('a -> 'b t) -> 'b t
+end
+
+module Maybe : Monad = struct
+  type 'a t = 'a option
+
+  let return x = Some x
+
+  let ( >>= ) m f =
+    match m with
+    | Some x -> f x
+    | None -> None
+end
+
+open Maybe
+
+let add a b =
+  a >>= fun x ->
+    b >>= fun y ->
+      return (x + y)
