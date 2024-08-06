657e0883d27f1e74f2a155905297a53fae103627
qylWheels <command1748165360@126.com>
2024-08-06 14:34:58 +0000

    Finish task: fmap and join

--- Chapter8/Exercises/monad/src/monad.ml
+++ Chapter8/Exercises/monad/src/monad.ml
@@ -2,6 +2,8 @@ module type Monad = sig
   type 'a t
   val return : 'a -> 'a t
   val ( >>= ) : 'a t -> ('a -> 'b t) -> 'b t
+  val ( >>| ) : 'a t -> ('a -> 'b) -> 'b t
+  val join : 'a t t -> 'a t
 end
 
 module Maybe : Monad = struct
@@ -12,6 +14,17 @@ module Maybe : Monad = struct
   let ( >>= ) m f =
     match m with
     | Some x -> f x
+    | None -> None
+
+  let ( >>| ) m f =
+    match m with
+    | Some x -> Some (f x)
+    | None -> None
+
+  let join m =
+    match m with
+    | Some (Some x) -> Some x
+    | Some (None) -> None
     | None -> None
 end
 
